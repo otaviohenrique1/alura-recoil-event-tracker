2b9b742d82394c3884ee1f5d2d0eaa6d06d1c67e
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-10-31 17:59:20 -0300

    commit 8

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -1,13 +1,10 @@
-
 import React from 'react'
-// import { IEvento } from '../../interfaces/IEvento';
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
 import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
 import 'kalend/dist/styles/index.css';
-import { useRecoilValue } from 'recoil';
-import { listaDeEventosState } from '../../state/atom';
 import useAtualizarEvento from '../../state/hooks/useAtualizarEvento';
+import useListaDeEventos from '../../state/hooks/useListaDeEventos';
 
 interface IKalendEvento {
   id?: number
@@ -17,11 +14,13 @@ interface IKalendEvento {
   color: string
 }
 
-const Calendario: React.FC/* <{ eventos: IEvento[] }> */ = (/* { eventos } */) => {
+const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
 
-  const eventos = useRecoilValue(listaDeEventosState);
+  const eventos = useListaDeEventos();
+
+  const atualizarEvento = useAtualizarEvento();
 
   eventos.forEach(evento => {
     const chave = evento.inicio.toISOString().slice(0, 10)
@@ -36,8 +35,6 @@ const Calendario: React.FC/* <{ eventos: IEvento[] }> */ = (/* { eventos } */) =
       color: 'blue'
     })
   })
-
-  const atualizarEvento = useAtualizarEvento();
 
   const onEventDragFinish: OnEventDragFinish = (
     kalendEventoInalterado: CalendarEvent,
